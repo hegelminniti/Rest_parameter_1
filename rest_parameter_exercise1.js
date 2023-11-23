14a64a9d357c933a7bbf99a66c45ea739346aa37
Hegsina <hegelminniti@gmail.com>
2023-11-23 19:09:44 +0100

    Rest Parameter - Exercise 1

---
+++ rest_parameter_exercise1.js
@@ -0,0 +1,8 @@
+// Crea una funzione chiamata somma che accetta un numero qualsiasi di argomenti e restituisce la somma di tali argomenti.
+// Utilizza il parametro rest per raccogliere gli argomenti. -Stampa in console l'output della funzione somma.
+
+function somma(...numbers){
+    return numbers.reduce((a, number) => a + number, 0)
+}
+
+console.log(somma(2,3,4,5))
